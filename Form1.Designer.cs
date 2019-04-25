8097252a8443472732a6a89294ffe461db81d70d
KRPselvi <49963320+KRPselvi@users.noreply.github.com>
2019-04-25 09:21:13 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GridView1 = new System.Windows.Forms.DataGridView();
+            this.Btn6mthRate = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.DTFrom = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.GridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GridView1
+            // 
+            this.GridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridView1.Location = new System.Drawing.Point(12, 88);
+            this.GridView1.Name = "GridView1";
+            this.GridView1.Size = new System.Drawing.Size(1408, 690);
+            this.GridView1.TabIndex = 0;
+            // 
+            // Btn6mthRate
+            // 
+            this.Btn6mthRate.Location = new System.Drawing.Point(24, 59);
+            this.Btn6mthRate.Name = "Btn6mthRate";
+            this.Btn6mthRate.Size = new System.Drawing.Size(101, 23);
+            this.Btn6mthRate.TabIndex = 1;
+            this.Btn6mthRate.Text = "Show 1 Month Rate";
+            this.Btn6mthRate.UseVisualStyleBackColor = true;
+            this.Btn6mthRate.Click += new System.EventHandler(this.Btn6MthRate_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(131, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Show 10 Monthly Rate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // DTFrom
+            // 
+            this.DTFrom.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.DTFrom.Location = new System.Drawing.Point(24, 12);
+            this.DTFrom.Name = "DTFrom";
+            this.DTFrom.Size = new System.Drawing.Size(127, 20);
+            this.DTFrom.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1360, 742);
+            this.Controls.Add(this.DTFrom);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Btn6mthRate);
+            this.Controls.Add(this.GridView1);
+            this.Name = "Form1";
+            this.Text = "Test";
+            ((System.ComponentModel.ISupportInitialize)(this.GridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView GridView1;
+        //private System.Windows.Forms.DataGridViewTextBoxColumn BkName;
+        private System.Windows.Forms.Button Btn6mthRate;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker DTFrom;
+        //private System.Windows.Forms.DataGridViewTextBoxColumn BkName;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text.RegularExpressions;
+using System.Net;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DTFrom.MaxDate = DateTime.Now.AddMonths(-1);
+            DTFrom.MinDate = DateTime.Now.AddMonths(-1);
+        }
+
+        private void Btn6MthRate_Click(object sender, EventArgs e)
+        {
+            string strDTFrom;
+
+            strDTFrom = Convert.ToDateTime(DTFrom.Text).ToString("yyyy-MM");
+                        
+            string URL = @"https://eservices.mas.gov.sg/api/action/datastore/search.json?resource_id=5f2b18a8-0883-4769-a635-879c63d3caac&filters[end_of_month]=" + strDTFrom + "";
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+            WebRequest request = WebRequest.Create(URL);
+            // Get the response.
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            // Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.
+            Console.WriteLine(responseFromServer);
+            // Cleanup the streams and the response.
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+            int startIndex = responseFromServer.IndexOf("records");
+            startIndex = startIndex + 9;
+            int NoOfchars = responseFromServer.Length - (startIndex);
+            string stringToShow = responseFromServer.Substring(startIndex, NoOfchars);
+            DataTable dt = JsonStringToDataTable(stringToShow);
+            GridView1.DataSource = dt;
+        }
+
+        public DataTable JsonStringToDataTable(string jsonString)
+        {
+            DataTable dt = new DataTable();
+            string[] jsonStringArray = Regex.Split(jsonString.Replace("[", "").Replace("]", ""), "},{");
+            List<string> ColumnsName = new List<string>();
+            foreach (string jSA in jsonStringArray)
+            {
+                string[] jsonStringData = Regex.Split(jSA.Replace("{", "").Replace("}", ""), ",");
+                foreach (string ColumnsNameData in jsonStringData)
+                {
+                    try
+                    {
+                        int idx = ColumnsNameData.IndexOf(":");
+                        string ColumnsNameString = ColumnsNameData.Substring(0, idx - 1).Replace("\"", "");
+                        if (!ColumnsName.Contains(ColumnsNameString))
+                        {
+                            ColumnsName.Add(ColumnsNameString);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception(string.Format("Error Parsing Column Name : {0}", ColumnsNameData + ex.Message.ToString()));
+                    }
+                }
+                break;
+            }
+            foreach (string AddColumnName in ColumnsName)
+            {
+                dt.Columns.Add(AddColumnName);
+            }
+            foreach (string jSA in jsonStringArray)
+            {
+                string[] RowData = Regex.Split(jSA.Replace("{", "").Replace("}", ""), ",");
+                DataRow nr = dt.NewRow();
+                foreach (string rowData in RowData)
+                {
+                    try
+                    {
+                        int idx = rowData.IndexOf(":");
+                        string RowColumns = rowData.Substring(0, idx - 1).Replace("\"", "");
+                        string RowDataString = rowData.Substring(idx + 1).Replace("\"", "");
+                        nr[RowColumns] = RowDataString;
+                    }
+                    catch (Exception ex)
+                    {
+                        continue;
+                    }
+                }
+                dt.Rows.Add(nr);
+            }
+            return dt;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string strDTFrom, strDTTo;
+
+            strDTFrom = Convert.ToDateTime(DTFrom.Text).AddMonths(-10).ToString("yyyy-MM");
+            strDTTo = Convert.ToDateTime(DTFrom.Text).ToString("yyyy-MM");
+            string URL = @"https://eservices.mas.gov.sg/api/action/datastore/search.json?resource_id=5f2b18a8-0883-4769-a635-879c63d3caac&limit=10&between[end_of_month]="+strDTFrom+","+strDTTo+"";
+            ServicePointManager.Expect100Continue = true;
+            
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+            WebRequest request = WebRequest.Create(URL);
+            // Get the response.
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            // Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.
+            Console.WriteLine(responseFromServer);
+            // Cleanup the streams and the response.
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+            int startIndex = responseFromServer.IndexOf("records");
+            startIndex = startIndex + 9;
+            int NoOfchars = responseFromServer.Length - (startIndex);
+            string stringToShow = responseFromServer.Substring(startIndex, NoOfchars);
+            DataTable dt = JsonStringToDataTable(stringToShow);
+            GridView1.DataSource = dt;
+        }
+    }
+}
